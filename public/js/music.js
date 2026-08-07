6b6c186a33b7cba747a77c5f432bbfcda1a5196e
fetchthesauce <rukjaa908@gmail.com>
2026-08-07 23:32:27 +0530

    improve mini player UI and optimize music UI for mobile devices

--- public/js/music.js
+++ public/js/music.js
@@ -318,10 +318,6 @@ window.MusicPlayer = {
     const m = Math.floor(secs / 60);
     const s = Math.floor(secs % 60);
     return `${m}:${s.toString().padStart(2, '0')}`;
-  },
-  
-  close() {
-    document.getElementById('music-drawer').classList.remove('open');
   }
 };
 
